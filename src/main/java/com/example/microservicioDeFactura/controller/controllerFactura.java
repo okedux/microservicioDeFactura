6246b5a3a610deb49d2e62328cfe2f8427b5125d
okedux <be.ojedao@duocuc.cl>
2025-05-13 00:40:07 -0400

    a real

--- src/main/java/com/example/microservicioDeFactura/controller/controllerFactura.java
+++ src/main/java/com/example/microservicioDeFactura/controller/controllerFactura.java
@@ -1,5 +1,38 @@
 package com.example.microservicioDeFactura.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.microservicioDeFactura.model.factura;
+import com.example.microservicioDeFactura.service.facturaService;
+
+@RestController
+@RequestMapping("/api/factura/v1")
 public class controllerFactura {
 
+    @Autowired
+    private factura nuevaFactura;
+
+    @Autowired
+    private facturaService facturaService;
+    @GetMapping("/listar")
+    public List<factura> listarFacturas() {
+        return facturaService.listarFacturas();
+    }
+    @GetMapping("/buscarPorRut/{rutCliente}")
+    public List<factura> buscarPorRut(@PathVariable String rutCliente) {
+         return facturaService.buscarPorRut(rutCliente);
+    }
+    @GetMapping("/buscarPorId/{idBuscar}")
+    public List<factura> buscarPorId(@PathVariable String idBuscar) {
+        return facturaService.buscarPorId(idBuscar);
+    }
+    @GetMapping("/eliminarPorId/{idEliminar}")
+    public List<factura> eliminarPorId(@PathVariable String idEliminar) {
+        return facturaService.eliminarPorId(idEliminar);
+    }
 }
