6d138cbed82bec2861b3efed894af4592f364546
Fernanda Lazo <ferlazoib@gmail.com>
2025-05-30 09:08:37 -0400

    anotaciones

--- src/main/java/com/example/microservicioDeFactura/repository/ClienteRepository.java
+++ src/main/java/com/example/microservicioDeFactura/repository/ClienteRepository.java
@@ -4,6 +4,8 @@ import com.example.microservicioDeFactura.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     Cliente findByRutEmpresa(String rutEmpresa);
