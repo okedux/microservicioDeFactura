b8061a7051bbd9360a2c97b6bbe4fd7e1d0d8da5
okedux <be.ojedao@duocuc.cl>
2025-06-24 13:19:09 -0400

    webiwabo

--- src/main/java/com/example/microservicioDeFactura/config/SwaggerConfig.java
+++ src/main/java/com/example/microservicioDeFactura/config/SwaggerConfig.java
@@ -1,18 +1,20 @@
 package com.example.microservicioDeFactura.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class SwaggerConfig {
+    
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("Microservicio de Factura API")
-                .version("1.0")
-                .description("API para gestionar facturas en el microservicio de factura."));
+                .info(new Info()
+                        .title("ZeedeByte's API")
+                        .version("2.0")
+                        .description("Documentación de la API de residuos para ZeedeByte"));
     }
 }
